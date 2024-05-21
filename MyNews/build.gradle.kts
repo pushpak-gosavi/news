ee7131f06a245a94e883f74175069cd09bc426e5
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2024-05-21 17:59:19 +0530

    master:Stable, Implemet the myNews Library

--- MyNews/build.gradle.kts
+++ MyNews/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+//    id ("kotlin-kapt")
+//    id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -41,4 +43,18 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//    //Dagger - Hilt
+//    implementation ("com.google.dagger:hilt-android:2.48")
+//    kapt ("com.google.dagger:hilt-android-compiler:2.48")
+//    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+//    kapt ("androidx.hilt:hilt-compiler:1.2.0")
+//    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+//
+//
+//
+//    // Retrofit
+//    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+//    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+//    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
 }
+
--- app/src/main/java/com/pushpak/newssdk/MainActivity.kt
+++ app/src/main/java/com/pushpak/newssdk/MainActivity.kt
@@ -3,13 +3,22 @@ package com.pushpak.newssdk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pushpak.mynews.Addition
 import com.pushpak.mynews.addTwoNumber
 import com.pushpak.newssdk.ui.theme.NewsSDKTheme
@@ -24,9 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                  val add =  Addition(10,20)
-                    println(add.addTwoNumber())
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -34,17 +41,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var addition by remember {
+        mutableIntStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Addition of two number is $addition",
+            modifier = modifier.padding(bottom = 20.dp)
+        )
+        Button(onClick = {
+            addition = Addition(10, 20).addTwoNumber()
+        }) {
+            Text(text ="Call me")
+        }
+    }
 }
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NewsSDKTheme {
-        Greeting("Android")
+        Greeting()
     }
-}
+}*/
